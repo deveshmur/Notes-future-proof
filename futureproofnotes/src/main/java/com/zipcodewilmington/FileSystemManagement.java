43b84a4cb641e0d007451df43abb3323740b413f
deveshmur <dmdevesh@gmail.com>
2025-11-18 15:17:20 -0500

    directory and file path added in filesystemmanagement

--- futureproofnotes/src/main/java/com/zipcodewilmington/FileSystemManagement.java
+++ futureproofnotes/src/main/java/com/zipcodewilmington/FileSystemManagement.java
@@ -1,5 +1,36 @@
 package com.zipcodewilmington;
 
+import java.nio.file.Path;
+import java.util.List;
+
+
 public class FileSystemManagement {
     
+    private final Path notesDirectory;
+
+    
+    public FileSystemManagement (Path notesDirectory) {
+        this.notesDirectory = notesDirectory;
+    }
+
+    public void ensureNotesDirectory() {
+    }
+
+    public List<Path> listNoteFiles() {
+        return List.of();
+    }
+
+    public boolean deleteFile (Path filePath) {
+        return false;
+    }
+
+    public Path resolveNotePath(String noteId) {
+        return null;
+    }
+
+    public void openInEditor(Path filePath) {
+    }
+
+
+
 }
