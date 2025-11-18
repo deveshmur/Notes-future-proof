a9a1ee99e1a8cf7e66b09cdc42b7c9cf65f36e8a
deveshmur <dmdevesh@gmail.com>
2025-11-18 15:07:43 -0500

    implemented tag for later use

--- futureproofnotes/src/main/java/com/zipcodewilmington/Note.java
+++ futureproofnotes/src/main/java/com/zipcodewilmington/Note.java
@@ -18,8 +18,8 @@ public class Note {
     public String getBody() { return body; }
     public void setBody(String body) { this.body = body; }
 
-    public void updateModified() {
+    public void updateModified() { 
     }
 
-    
+
 }
--- futureproofnotes/src/main/java/com/zipcodewilmington/Tag.java
+++ futureproofnotes/src/main/java/com/zipcodewilmington/Tag.java
@@ -2,4 +2,16 @@ package com.zipcodewilmington;
 
 public class Tag {
     
+    private String name;
+    
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
