860b91e983fb6783f2c05156558e6b2a9d5003d5
deveshmur <dmdevesh@gmail.com>
2025-11-18 16:08:54 -0500

    added load, find, save, update, delete, and generate in NoteRepository

--- futureproofnotes/src/main/java/com/zipcodewilmington/NoteRepository.java
+++ futureproofnotes/src/main/java/com/zipcodewilmington/NoteRepository.java
@@ -1,8 +1,37 @@
 package com.zipcodewilmington;
 
+import java.util.List;
+import java.util.Optional;
+
 public class NoteRepository {
 
+    private final FileSystemManagement fileSystemManagement;
+    private final NoteFileParser parser;
 
+    public NoteRepository(FileSystemManagement fileSystemManagement, NoteFileParser parser) {
+        this.fileSystemManagement = fileSystemManagement;
+        this.parser = parser;
+    }
 
+    public List<Note> loadAll() {
+        return List.of();
+    }
 
+    public Optional<Note> Optional() {
+        return Optional.empty();
+    }
+
+    public void save (Note note) {
+    }
+
+    public void update (Note note) {
+    }
+
+    public boolean delete(String id) {
+        return false;
+    }
+
+    public String generateNoteId(String title) {
+        return "";
+    }
 }
