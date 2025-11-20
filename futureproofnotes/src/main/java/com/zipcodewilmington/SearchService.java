25840929eb757b3bcca4cec18f8a03b9eacecc1d
deveshmur <dmdevesh@gmail.com>
2025-11-20 13:47:04 -0500

    searchservice implemented to work with repository and note

--- futureproofnotes/src/main/java/com/zipcodewilmington/SearchService.java
+++ futureproofnotes/src/main/java/com/zipcodewilmington/SearchService.java
@@ -1,5 +1,30 @@
 package com.zipcodewilmington;
 
+import notes.domain.Note; 
+import notes.storage.NoteRepository;
+
+import java.util.List;
+
+
 public class SearchService {
     
+    private final NoteRepository repository; 
+
+    public SearchService(NoteRepository repository) {
+        this.repository = repository; 
+
+    }
+    public List<Note> searchByKeyword(String keyword) {
+        return List.of();
+    }
+
+    public List<Note> searchByTag(String keyword) {
+        return List.of();
+    } 
+
+    public boolean match(Note not, String keyword) {
+        return false; 
+    }
+
+
 }
--- futureproofnotes/src/main/java/com/zipcodewilmington/StatisticsService.java
+++ futureproofnotes/src/main/java/com/zipcodewilmington/StatisticsService.java
@@ -1,5 +1,12 @@
 package com.zipcodewilmington;
 
+import notes.domain.Note;
+import notes.storage.NoteRepository;
+
+import java.util.List;
+import java.util.Map;
+
+
 public class StatisticsService {
     
 }
