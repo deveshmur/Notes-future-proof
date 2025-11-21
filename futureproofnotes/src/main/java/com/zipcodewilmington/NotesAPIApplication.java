be64d7f11d1d9dd11d78dc55cefb470ff8dd8d19
deveshmur <dmdevesh@gmail.com>
2025-11-21 00:10:01 -0500

    created running spring boot application

---
+++ futureproofnotes/src/main/java/com/zipcodewilmington/NotesAPIApplication.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAPIApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(NotesAPIApplication.class, args);
+    }
+}
